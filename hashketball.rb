c03ba550b53b8a119688f742b56c293ee5e1b651
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-20 03:30:35 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,3 +145,13 @@ def num_points_scored(name)
     end
   end
 end
+
+def shoe_size(name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player_name, player_stat|
+      if name == player_name
+       return player_stat[:shoe]
+      end
+    end
+  end
+end
