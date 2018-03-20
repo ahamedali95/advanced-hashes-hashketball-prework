8de80411a2498dbb081bf5e372c568220d77cc7f
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-20 03:40:57 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,3 +173,15 @@ def team_names()
 
   teams
 end
+
+def player_numbers(name)
+  numbers = []
+
+  game_hash().each do |location, team_data|
+    team_data[:players].each do |player_name, player_stats|
+      numbers << player_stats[:number]
+    end
+  end
+
+  return numbers
+end
