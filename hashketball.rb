b3a91575abf525b4997fde19154be09063c27a4f
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-20 03:03:29 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,9 +6,8 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
+      players: {
+        "Alan Anderson" => {
           number: 0,
           shoe: 16,
           points: 22,
@@ -17,8 +16,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
-        }, {
-          player_name: "Reggie Evans",
+      },
+        "Reggie Evans" => {
           number: 30,
           shoe: 14,
           points: 12,
@@ -27,8 +26,8 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7
-        }, {
-          player_name: "Brook Lopez",
+        },
+        "Brook Lopez" => {
           number: 11,
           shoe: 17,
           points: 17,
@@ -37,8 +36,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
-        }, {
-          player_name: "Mason Plumlee",
+        },
+        "Mason Plumlee" => {
           number: 1,
           shoe: 19,
           points: 26,
@@ -47,8 +46,8 @@ def game_hash
           steals: 3,
           blocks: 8,
           slam_dunks: 5
-        }, {
-          player_name: "Jason Terry",
+        },
+        "Jason Terry" => {
           number: 31,
           shoe: 15,
           points: 19,
@@ -58,14 +57,13 @@ def game_hash
           blocks: 11,
           slam_dunks: 1
         }
-      ]
+      }
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
+      players: {
+        "Jeff Adrien" => {
           number: 4,
           shoe: 18,
           points: 10,
@@ -74,8 +72,8 @@ def game_hash
           steals: 2,
           blocks: 7,
           slam_dunks: 2
-        }, {
-          player_name: "Bismak Biyombo",
+        },
+        "Bismak Biyombo" => {
           number: 0,
           shoe: 16,
           points: 12,
@@ -84,8 +82,8 @@ def game_hash
           steals: 7,
           blocks: 15,
           slam_dunks: 10
-        }, {
-          player_name: "DeSagna Diop",
+        },
+        "DeSagna Diop" => {
           number: 2,
           shoe: 14,
           points: 24,
@@ -94,8 +92,8 @@ def game_hash
           steals: 4,
           blocks: 5,
           slam_dunks: 5
-        }, {
-          player_name: "Ben Gordon",
+        },
+        "Ben Gordon" => {
           number: 8,
           shoe: 15,
           points: 33,
@@ -104,8 +102,8 @@ def game_hash
           steals: 1,
           blocks: 1,
           slam_dunks: 0
-        }, {
-          player_name: "Brendan Haywood",
+        },
+        "Brendan Haywood" => {
           number: 33,
           shoe: 15,
           points: 6,
@@ -115,25 +113,30 @@ def game_hash
           blocks: 5,
           slam_dunks: 12
         }
-      ]
+      }
     }
   }
 end
 
-def good_practices
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    #binding.pry
-      team_data.each do |attribute, data|
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-        #binding.pry
+# def good_practices
+#   game_hash.each do |location, team_data|
+#     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+#     binding.pry
+#       team_data.each do |attribute, data|
+#         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+#         binding.pry
+# 
+#         #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+#         data.each do |data_item|
+#             binding.pry
+#       end
+#     end
+#   end
+# end
+# 
+# good_practices()
 
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-        data.each do |data_item|
-            binding.pry
-      end
-    end
-  end
+def num_points_scored(player_name)
+  
 end
-
-good_practices()
+  
