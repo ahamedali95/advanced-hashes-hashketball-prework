02df6548d0f8de3010137f0ffd2c5a8ff76c4b57
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-20 03:03:33 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,7 +125,7 @@ end
 #       team_data.each do |attribute, data|
 #         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
 #         binding.pry
-# 
+#
 #         #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
 #         data.each do |data_item|
 #             binding.pry
@@ -133,10 +133,9 @@ end
 #     end
 #   end
 # end
-# 
+#
 # good_practices()
 
 def num_points_scored(player_name)
-  
+
 end
-  
